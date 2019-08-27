415cefb522c3ed98de39afb1566fb1c3763ef4fc
Jason Lewallen <jklewa@gmail.com>
2019-08-27 15:56:08 -0600

    Missing eps and recipes, added pg_dump to update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,7 @@ wget localhost:5000/guests?format=json -O ibdb.guests.json -nv
 wget localhost:5000/recipes?format=json -O ibdb.recipes.json -nv
 wget localhost:5000/references?format=json -O ibdb.references.json -nv
 wget localhost:5000/shows?format=json -O ibdb.shows.json -nv
+
+pg_dump --dbname=babish_db --create --file=./ibdb/db_dump.sql --username=postgres --host=localhost --port=54320
+
 echo "Done"
