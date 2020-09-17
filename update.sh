372090461be6207272a7e1651609bf8bf4151e8c
Jason Lewallen <jklewa@gmail.com>
2020-09-16 23:00:44 -0600

    update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ wget localhost:5000/recipes?format=json -O ibdb.recipes.json -nv
 wget localhost:5000/references?format=json -O ibdb.references.json -nv
 wget localhost:5000/shows?format=json -O ibdb.shows.json -nv
 
-pg_dump --dbname=babish_db --create --file=./ibdb/db_dump.sql --username=postgres --host=localhost --port=54320
+PGPASSWORD=${POSTGRES_PASSWORD} PGUSER=${POSTGRES_USERNAME:-postgres} \
+  PGHOST=${POSTGRES_HOSTNAME:-localhost} PGPORT=${POSTGRES_PORT:-54320} \
+  pg_dump --dbname=babish_db --create --file=./ibdb/db_dump.sql
 
 echo "Done"
