3d4b74cdbfece0953910aae13c3eee2e94bb06ec
Jason Lewallen <jklewa@gmail.com>
2020-09-27 19:49:56 -0600

    Restucturing repo

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 set -ex
-wget localhost:5000/episodes?format=json -O ibdb.episodes.json -nv
-wget localhost:5000/guests?format=json -O ibdb.guests.json -nv
-wget localhost:5000/recipes?format=json -O ibdb.recipes.json -nv
-wget localhost:5000/references?format=json -O ibdb.references.json -nv
-wget localhost:5000/shows?format=json -O ibdb.shows.json -nv
+wget localhost:5000/episodes?format=json -O datasets/ibdb.episodes.json -nv
+wget localhost:5000/guests?format=json -O datasets/ibdb.guests.json -nv
+wget localhost:5000/recipes?format=json -O datasets/ibdb.recipes.json -nv
+wget localhost:5000/references?format=json -O datasets/ibdb.references.json -nv
+wget localhost:5000/shows?format=json -O datasets/ibdb.shows.json -nv
 
 PGPASSWORD=${POSTGRES_PASSWORD} PGUSER=${POSTGRES_USERNAME:-postgres} \
   PGHOST=${POSTGRES_HOSTNAME:-localhost} PGPORT=${POSTGRES_PORT:-54320} \
