57f317ac27243b88b325e980f66c5c6416eb2c9c
Jason Lewallen <jklewa@gmail.com>
2020-10-16 00:46:40 -0600

    Babish module pt2a module setup (#59)
    * setup.py for ibdb module and cli
    
    * Using ibdb module path; click cli
    
    * Rework deps into setup.py; requirements.txt only for local dev now
    
    * flake8

---
+++ ibdb/cli.py
@@ -0,0 +1,15 @@
+import os
+
+import click
+from ibdb.api import app
+
+
+@app.cli.command('test')
+@click.argument('foo')
+def test(foo):
+    print(foo)
+
+
+def main():
+    os.environ['FLASK_APP'] = 'ibdb.api'
+    return app.cli.main()
---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = [
+    "beautifulsoup4",
+    "click",
+    "flask",
+    "flask-sqlalchemy",
+    "furl",
+    "html5lib",
+    "measurement",
+    "psycopg2-binary",
+    "pyfunctional",
+    "requests",
+]
+
+setuptools.setup(
+    name="ibdb",
+    version="0.0.1",
+    license='GPLv3+',
+    author="jklewa",
+    author_email="jklewa@gmail.com",
+    description="Data with Babish DB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jklewa/data-with-babish",
+    scripts=[
+        "ibdb/bin/ibdb",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
