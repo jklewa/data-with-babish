6d9380c57cabd0d1f1e2f0080d7e0c59b249aad1
Jason Lewallen <jklewa@gmail.com>
2021-01-04 11:11:32 -0700

    basic test for export.py

--- tests/ibdb/test_export.py
+++ tests/ibdb/test_export.py
@@ -1,10 +1,15 @@
 import unittest
+import os
+import tempfile
+
+from ibdb.export import write_response
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+class TestExport(unittest.TestCase):
+    def test_write_response(self):
+        route = '/episodes'
+        to = 'episodes.json'
+        with tempfile.TemporaryDirectory() as tempdir:
+            out_filepath = os.path.join(tempdir, to)
+            write_response(route, out_filepath)
+            self.assertTrue(os.path.exists(out_filepath))
